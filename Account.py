53542cc31f75f17a8bf7285f763b6ce5b5ea6b1f
harouna <harounathaim2005@gmail.com>
2023-04-19 23:07:45 -0400

    Created initial function-based add row and dump table SQL functionality, added Elite101 chatbot prototype scripts

---
+++ Account.py
@@ -0,0 +1,84 @@
+class account:
+
+  '''
+This is the `account` class that acts as the foundation the chatbot. An instance of this class will initialize with a balance of 0 unless otherwise specified; 
+other attributes having to be passed in as arguments upon declaration. Main functionality of word interpretation takes place in the class. 
+Interpret functions iterates through the list of "request variables", taking the threshold—number of words needing to be present in the phrase 
+passed as an argument into the method for it to satisfy—and needed words in order to identify which option the user would like to choose.
+  '''
+  
+  # initialize all private instance variables
+  def __init__(self, username, password, balance=None):
+    self.username = username
+    self.password = password
+    self.balance = 0
+    self.authenticate = False
+    self.request_vars = [["change", "password", 2], ["see", "show", "balance", 2], ["add", "balance", 2], ["display", "show", "account", "info", 3], 
+    ["display", "show", "accounts", 2], ["add", "account", 2], ["change", "account", 2], ["quit", "exit", "leave", 1]]
+    self.current_input = ""
+    confidence = 0
+    threshold = 2
+    self.threshold = threshold
+    self.confidence = confidence
+
+  # accessor and mutator methods
+  def add_balance(self, add):
+    self.balance += add
+
+  def get_user(self):
+    return self.username
+
+  def get_pass(self):
+    return self.password
+
+  def get_pass_mutate(self):
+    pass_arr = [i for i in self.password]
+    if len(pass_arr) == 1:
+      pass_arr[0] = "*"
+    elif len(pass_arr) <= 2:
+      pass_arr[1] = "*"
+    else:
+      for i in range(1, len(pass_arr) - 1):
+        pass_arr[i] = "*"
+    return "".join(pass_arr)
+
+  def get_balance(self):
+    return self.balance
+
+  def get_auth(self):
+    return self.authenticate
+
+  def set_user(self, new_user):
+    self.username = new_user
+
+  def set_pass(self, new_pass):
+    self.password = new_pass
+
+  def set_balance(self, new_balance):
+    self.balance = new_balance
+
+  def set_auth(self, new_auth):
+    self.authenticate = new_auth
+
+  def set_current_input(self, new_ans):
+    self.current_input = new_ans
+
+  def debug(self):
+    print(self.interpret)
+    print(self.current_input)
+
+  # crude language interpretation function
+  def interpret(self, word):
+    lword = word.lower()
+    words = lword.split()
+    confidence = 0
+    threshold = 2
+    for list in self.request_vars:
+      threshold = list[len(list) - 1]
+      print(threshold)
+      for i in list:
+        if i in words:
+          confidence += 1
+          if confidence >= threshold:
+            value = self.request_vars.index(list) + 1
+            return str(value)
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import mysql.connector
 
+def insert(connection, user, password, email, table):
+    print("debug", user , password , email)
+    testQuery = (f'INSERT INTO {table} (username, password_hash, email) VALUES ("{user}", "hashed_{password}", "{email}");')
+    cursor = connection.cursor()
+    cursor.execute(testQuery)
+    connection.commit()
+    cursor.close()
+
+def dump_table(connection):
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT * FROM {table}')
+    for item in cursor:
+        print(item)
+    cursor.close()
+
 connection = mysql.connector.connect(user = 'root', database = 'new_schema', password = 'Sonic46!!')
+table = 'accounts2'
 
-cursor = connection.cursor()
-
-testQuery = ("SELECT username FROM accounts")
-
-cursor.execute(testQuery)
-
-for item in cursor:
-    print(item)
-
-cursor.close()
+insert(connection, 'hthiam', 'Sonic55', 'ht@gmail.com', table)
+dump_table(connection)
 
 connection.close()
 
