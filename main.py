0b793f0df2513ae569e00f1d3fec451ef15f84a6
harouna <harounathaim2005@gmail.com>
2023-04-27 09:00:46 -0400

    Added major contributions including most sql update and data mutation methods

--- main.py
+++ main.py
@@ -1,37 +1,67 @@
-import mysql.connector
+from sqlfunc import *
+from menu import menu
 
-def insert_row(connection, user, password, email, table):
-    cursor = connection.cursor()
-    cursor.execute(f'INSERT INTO {table} (username, password_hash, email) VALUES ("{user}", "hashed_{password}", "{email}");')
-    connection.commit()
-    cursor.close()
+print(get_data(connection, 'balance', 1))
 
-def remove_row(connection, id, table):
-    print("debug", id, table)
-    cursor = connection.cursor()
-    cursor.execute(f'DELETE FROM {table} WHERE id{table} = {id};')
-    connection.commit()
-    cursor.close()
+user = input("Enter your username:")
+password = input("Enter your password:")
 
-def clear_table(connection, table):
-    print("debug", id, table)
-    cursor = connection.cursor()
-    cursor.execute(f'DELETE FROM {table};')
-    connection.commit()
-    cursor.close()
+while check_user_and_pass(connection, user, password) == False:
+    print("")
+    print("Username or password is incorrect. Please enter them again.")
+    user = input("Username: ")
+    password = input("Password: ")
+    print("")
 
-def dump_table(connection):
-    cursor = connection.cursor()
-    cursor.execute(f'SELECT * FROM {table}')
-    for item in cursor:
-        print(item)
-    cursor.close()
+"""
+get_data:
+cursor = connection.cursor()
+cursor.execute(f'SELECT balance FROM bank_accounts WHERE idbank_accounts = "{id}";')
+item = get(cursor)
+cursor.close()
+print(item)
+"""
 
-connection = mysql.connector.connect(user = 'root', database = 'new_schema', password = 'Sonic46!!')
-table = 'accounts2'
+print("Welcome...")
+id = get_id(connection, password)
+print(id)
 
-insert_row(connection, 'hthiam', 'Sonic46!!', 'hthiam252', table)
-dump_table(connection)
+print("")
+on = True
+while on:
+    print("")
+    print("(1) Get balance")
+    print("(2) Add to balance")
+    print("(3) Change username")
+    print("(4) Change password")
+    print("(5) Add account")
+    print("(6) Switch account")
+    print("(7) Transfer balance")
+    print("(8) Quit")
+    print("")
+    decision = input("Tell me, what would you like to do? ")
+    print("")
+    if decision == "1":
+        balance = get_data(connection, 'balance', id)
+        print(balance)
+    elif decision == "2":
+        increment_balance(connection, id)
+    elif decision == "3":
+        pass
+    elif decision == "4":
+        pass
+    elif decision == "5":
+        pass
+    elif decision == "6":
+        pass
+    elif decision == "7":
+        pass
+    elif decision == "8":
+        print("Closing...")
+        print("")
+        break
+    else:
+        print("Please choose from the displayed options. ")
+        print("")
 
 connection.close()
-
---
+++ sqlfunc.py
@@ -0,0 +1,75 @@
+import mysql.connector
+
+def insert_row(connection, user, password, email, balance):
+    cursor = connection.cursor()
+    #Encrypt class (?) -->
+    cursor.execute(f'INSERT INTO bank_accounts (user, pass, email, balance) VALUES ("{user}", "{password}", "{email}", "{balance}");')
+    connection.commit()
+    cursor.close()
+
+def remove_row(connection, id):
+    print("debug", id)
+    cursor = connection.cursor()
+    cursor.execute(f'DELETE FROM bank_accounts WHERE idbank_accounts = {id};')
+    connection.commit()
+    cursor.close()
+
+def clear_table(connection):
+    print("debug", id)
+    cursor = connection.cursor()
+    cursor.execute(f'DELETE FROM bank_accounts;')
+    connection.commit()
+    cursor.close()
+
+def dump_table(connection):
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT * FROM bank_accounts;')
+    for item in cursor:
+        print(item)
+    cursor.close()
+
+def get_id(connection, password):
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT idbank_accounts FROM bank_accounts WHERE pass = "{password}";')
+    id = get(cursor)
+    cursor.close()
+    return id
+
+def get_data(connection, field, id):
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT {field} FROM bank_accounts WHERE idbank_accounts = "{id}";')
+    item = get(cursor)
+    cursor.close()
+    return item
+
+def get(cursor):
+    for i in cursor:
+        for x in i:
+            return x
+
+def update(connection, field, new_value, id):
+    cursor = connection.cursor()
+    cursor.execute(f'UPDATE bank_accounts SET {field} = {new_value} WHERE idbank_accounts = "{id}";')
+    connection.commit()
+    cursor.close()
+
+def increment_balance(connection, id):
+    value = input("How much would you like to add to your account? ")
+    print("")
+    new_balance = int(get_data(connection, 'balance', id)) + int(value)
+    update(connection, 'balance', f'{new_balance}', id)
+    print(f"Your new balance is {new_balance}.")
+
+def check_user_and_pass(connection, user, password):
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT COUNT(*) FROM bank_accounts WHERE pass = "{password}";')
+    pass_check = get(cursor)
+    cursor.execute(f'SELECT COUNT(*) FROM bank_accounts WHERE user = "{user}";')
+    user_check = get(cursor)
+    cursor.close()
+    if pass_check == 1 and user_check == 1:
+        return True
+    else:
+        return False
+
+connection = mysql.connector.connect(user = 'root', database = 'new_schema', password = 'Sonic46!!')
